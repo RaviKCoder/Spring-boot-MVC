4971bb8569cf0dbf822c2164ba824708b7298569
Ravi kumar <rsravi433399@gmail.com>
2026-02-11 00:52:20 +0530

    Student management system by MVC proj added

---
+++ BootProj68-SpbMVCProj11-DevelopSpringMVC-CRUD-StudentManagementSystem/src/main/java/com/ravi/service/StudentServiceImpl.java
@@ -0,0 +1,54 @@
+package com.ravi.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ravi.entity.StudentEntity;
+import com.ravi.repository.IStudentRepository;
+
+@Service
+public class StudentServiceImpl implements IStudentService {
+	
+	@Autowired
+	IStudentRepository studentRepo;
+
+	@Override
+	public String saveStudent(StudentEntity entity) {
+		//use repository
+		StudentEntity savedStudent = studentRepo.save(entity);
+		
+		return "Student saved successfully with id: "+savedStudent.getId();
+	}
+
+	@Override
+	public List<StudentEntity> showAllStudent() {
+		List<StudentEntity> listEntity = studentRepo.findAll();
+		return listEntity;
+	}
+
+	@Override
+	public StudentEntity showStudentById(Integer id) {
+		StudentEntity entity = studentRepo.findById(id).orElseThrow(()->new IllegalArgumentException("Invalid student id"));
+		return entity;
+	}
+
+	@Override
+	public String editStudent(StudentEntity entity) {
+		StudentEntity save = studentRepo.save(entity);
+		return "Student updated successfully with id: "+save.getId();
+	}
+
+	@Override
+	public String deleteStudentById(Integer id) {
+		boolean  flag= studentRepo.existsById(id);
+		if(flag) {
+			studentRepo.deleteById(id);
+			return "Student deleted successfully with id: "+id;
+		}
+		return "Student not found with id: "+id;
+	}
+
+}
